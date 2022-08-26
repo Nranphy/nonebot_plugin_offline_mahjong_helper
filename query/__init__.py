534383b74789bd601b0fda6e747908db3435da59
Nranphy <3102002900@qq.com>
2022-08-26 22:50:30 +0800

    Feat: 增加了雀魂查询详细数据，用图片发送，添加了css样式 Fix: 改变计算相关函数的位置至相应子模块

--- query/__init__.py
+++ query/__init__.py
@@ -2,15 +2,18 @@
 
 
 
-from nonebot import on_command, require, get_bot, get_driver
+from nonebot import on_command
 from nonebot.typing import T_State
 from nonebot.params import State, CommandArg
-from nonebot.adapters.onebot.v11 import Bot, Event, MessageSegment, Message
+from nonebot.adapters.onebot.v11 import Bot, Event, Message, MessageSegment
 from nonebot.log import logger
 
 import httpx
 import json
 import time
+from pathlib import Path
+
+from nonebot_plugin_htmlrender import md_to_pic
 
 from .utils import *
 
@@ -19,7 +22,7 @@ from .utils import *
 
 majsoul_query = on_command("雀魂查询",aliases={"查询雀魂","雀魂战绩"},priority=10,block=True)
 
-# 天凤查询网站都有墙，而且多数群玩天凤的应该都不算多
+# 暂时没写
 # tenhou_query = on_command("天凤查询",aliases={"查询天凤","天凤战绩"},priority=10,block=True)
 
 
@@ -30,20 +33,28 @@ majsoul_query = on_command("雀魂查询",aliases={"查询雀魂","雀魂战绩"
 @majsoul_query.handle()
 async def majsoul_query_ensure_index(bot: Bot, event: Event, state:T_State=State(), arg:Message=CommandArg()):
     name = arg.extract_plain_text().strip()
-    user_info = json.loads(httpx.get(f"https://ak-data-1.sapk.ch/api/v2/pl4/search_player/{name}?limit=5").content)
-    state["user_info"] = user_info
-    if not len(user_info):
-        await majsoul_query.finish(f"未找到名称包含【{name}】的玩家...\n雀魂查询数据来自雀魂牌谱屋，仅可查询段位金之间、玉之间及王座之间的牌谱。\nps.暂不支持三麻查询",at_sender=True)
-    elif len(user_info)==1:
+    user_search_pl4 = json.loads(httpx.get(f"https://4.data.amae-koromo.com/api/v2/pl4/search_player/{name}?limit=3").content)
+    user_search_pl3 = json.loads(httpx.get(f"https://4.data.amae-koromo.com/api/v2/pl3/search_player/{name}?limit=3").content)
+    user_id = []
+    user_search = []
+    for user in user_search_pl4+user_search_pl3:
+        if user["id"] not in user_id:
+            user_search.append(user)
+            user_id.append(user["id"])
+    state["user_info"] = user_search
+
+    if not user_search:
+        await majsoul_query.finish(f"未找到名称包含【{name}】的玩家...\n雀魂查询数据来自雀魂牌谱屋，仅可查询段位金之间、玉之间及王座之间的牌谱。",at_sender=True)
+    elif len(user_search)==1:
         state["index"] = 0
     else:
-        user_info_message = []
-        for i in range(len(user_info)):
-            temp_level_info = majsoul_level_count(user_info[i]['level']['id'], user_info[i]['level']['score'], user_info[i]['level']['delta'])
-            temp_str = f"【{i+1}】 {user_info[i]['nickname']} \n（{temp_level_info[0]} [{temp_level_info[1]}/{temp_level_info[2]}]）\n最后对局时间：{time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(user_info[i]['latest_timestamp']))}"
-            user_info_message.append(temp_str)
+        user_search_message = []
+        for i in range(len(user_search)):
+            temp_level_info = majsoul_level_count(user_search[i]['level']['id'], user_search[i]['level']['score'], user_search[i]['level']['delta'])
+            temp_str = f"【{i+1}】 {user_search[i]['nickname']} \n（{temp_level_info[0]} [{temp_level_info[1]}/{temp_level_info[2]}]）\n最后对局时间：{time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(user_search[i]['latest_timestamp']))}"
+            user_search_message.append(temp_str)
         
-        await majsoul_query.send(f"查找到多个相关用户~\n直接回复对应序号进行该角色查询√\n=========\n"+'\n======\n'.join(user_info_message)+"\n=========",at_sender=True)
+        await majsoul_query.send(f"查找到多个相关用户~\n直接回复对应序号进行该角色查询√\n=========\n"+'\n======\n'.join(user_search_message)+"\n=========",at_sender=True)
 
 
 @majsoul_query.got("index")
@@ -62,32 +73,62 @@ async def majsoul_query_precess(bot: Bot, event: Event, state:T_State=State()):
     id = user_info[index]["id"]
     
     # 进行查询
-    url_basic = f"https://ak-data-6.pikapika.me/api/v2/pl4/player_stats/{id}/1262304000000/{int(time.time() * 1000)}?mode=16.12.9.15.11.8"
-    url_pro = f"https://ak-data-6.pikapika.me/api/v2/pl4/player_extended_stats/{id}/1262304000000/{int(time.time() * 1000)}?mode=16.12.9.15.11.8"
-    user_data_basic = json.loads(httpx.get(url_basic).content)
-    user_data_pro = json.loads(httpx.get(url_pro).content)
+    ## 四麻
+    url_basic_pl4 = f"https://4.data.amae-koromo.com/api/v2/pl4/player_stats/{id}/1262304000000/{int(time.time() * 1000)}?mode=16.12.9.15.11.8"
+    url_pro_pl4 = f"https://4.data.amae-koromo.com/api/v2/pl4/player_extended_stats/{id}/1262304000000/{int(time.time() * 1000)}?mode=16.12.9.15.11.8"
+    user_data_basic_pl4 = json.loads(httpx.get(url_basic_pl4).content)
+    user_data_pro_pl4 = json.loads(httpx.get(url_pro_pl4).content)
+    ## 三麻
+    url_basic_pl3 = f"https://4.data.amae-koromo.com/api/v2/pl3/player_stats/{id}/1262304000000/{int(time.time() * 1000)}?mode=26.24.22.25.23.21"
+    url_pro_pl3 = f"https://4.data.amae-koromo.com/api/v2/pl3/player_extended_stats/{id}/1262304000000/{int(time.time() * 1000)}?mode=26.24.22.25.23.21"
+    user_data_basic_pl3 = json.loads(httpx.get(url_basic_pl3).content)
+    user_data_pro_pl3 = json.loads(httpx.get(url_pro_pl3).content)
+
     # 基础数据处理
-    nickname = user_data_basic["nickname"]  # 游戏昵称
-    level_info = majsoul_level_count(user_data_basic["level"]["id"], user_data_basic["level"]["score"], user_data_basic["level"]["delta"]) # 段位信息
-    max_level_info = majsoul_level_count(user_data_basic["max_level"]["id"], user_data_basic["max_level"]["score"], user_data_basic["max_level"]["delta"]) # 最高段位信息
-    rank_retes = user_data_basic["rank_rates"] # 顺位率列表
-    avg_rank = user_data_basic["avg_rank"] # 平均顺位
-    negative_rate = user_data_basic["negative_rate"] # 被飞率
+    ## 四麻
+    if not user_data_basic_pl4.get("error",0):
+        nickname = user_data_basic_pl4["nickname"]  # 游戏昵称
+        level_info_pl4 = majsoul_level_count(user_data_basic_pl4["level"]["id"], user_data_basic_pl4["level"]["score"], user_data_basic_pl4["level"]["delta"]) # 段位信息
+        max_level_info_pl4 = majsoul_level_count(user_data_basic_pl4["max_level"]["id"], user_data_basic_pl4["max_level"]["score"], user_data_basic_pl4["max_level"]["delta"]) # 最高段位信息
+
+        level_str_pl4 = f"""
+====四麻====
+【记录段位】 {level_info_pl4[0]} [{level_info_pl4[1]}/{level_info_pl4[2]}]
+【最高段位】 {max_level_info_pl4[0]} [{max_level_info_pl4[1]}/{max_level_info_pl4[2]}]"""
+    else:
+        level_str_pl4 = ""
+    ## 三麻
+    if not user_data_basic_pl3.get("error",0):
+        nickname = user_data_basic_pl3["nickname"]  # 游戏昵称
+        level_info_pl3 = majsoul_level_count(user_data_basic_pl3["level"]["id"], user_data_basic_pl3["level"]["score"], user_data_basic_pl3["level"]["delta"]) # 段位信息
+        max_level_info_pl3 = majsoul_level_count(user_data_basic_pl3["max_level"]["id"], user_data_basic_pl3["max_level"]["score"], user_data_basic_pl3["max_level"]["delta"]) # 最高段位信息
+
+        level_str_pl3 = f"""
+====三麻====
+【记录段位】 {level_info_pl3[0]} [{level_info_pl3[1]}/{level_info_pl3[2]}]
+【最高段位】 {max_level_info_pl3[0]} [{max_level_info_pl3[1]}/{max_level_info_pl3[2]}]"""
+    else:
+        level_str_pl3 = ""
+
     # 进阶数据处理
-        # 可直接在user_data_pro中获取和牌率,自摸率,默听率,放铳率,副露率,立直率,平均打点,最大连庄,和了巡数,平均铳点,流局率,流听率,一发率,里宝率,被炸率,平均被炸点数,放铳时立直率,放铳时副露率,立直后放铳率,立直后非瞬间放铳率,副露后放铳率,立直后和牌率,副露后和牌率,立直后流局率,副露后流局率,放铳至立直,放铳至副露,放铳至默听,立直和了,副露和了,默听和了,立直巡目,立直收支,立直收入,立直支出,先制率,追立率,被追率,振听立直率,立直好型,立直多面,立直好型2,役满,最大累计番数,W立直,打点效率,铳点损失,净打点效率,平均起手向听,平均起手向听亲,平均起手向听子
+    data_pic = await data2pic(nickname,user_data_pro_pl4,user_data_pro_pl3)
+
     
-    await majsoul_query.finish(f"""
-【昵称】 {nickname}
-【记录段位】 {level_info[0]} [{level_info[1]}/{level_info[2]}]
-【最高段位】 {max_level_info[0]} [{max_level_info[1]}/{max_level_info[2]}]
-======
-【顺位率】
-1位：{rank_retes[0]:.2%}
-2位：{rank_retes[1]:.2%}
-3位：{rank_retes[2]:.2%}
-4位：{rank_retes[3]:.2%}
-平均顺位：{avg_rank:.2f}""",at_sender=True)
+    await majsoul_query.finish(MessageSegment.text(f"""
+【昵称】 {nickname}{level_str_pl4}{level_str_pl3}
+===详细信息===""")+MessageSegment.image(data_pic),at_sender=True)
 
 
-
-
+async def data2pic(nickname:str,userdata_pl4:dict,userdata_pl3:dict) -> bytes:
+    '''将两种进阶数据转化为Markdown，进而转为图片'''
+    data_keys = userdata_pl4.keys() if len(userdata_pl4)>=len(userdata_pl3) else userdata_pl3.keys()
+    mdstr = f"""<div class="nickname" style="font-size:20px" align="center"><b>{nickname}雀魂详细信息</b></div>
+| 统计数据 | 四麻 | 三麻 |
+| :----: | :---- | :---- |"""
+    for key in data_keys:
+        if key=='count':
+            mdstr += f"\n| **记录对局数** | {userdata_pl4.get(key,'未找到')} | {userdata_pl3.get(key,'未找到')} |"
+        elif key in ['最近大铳','id','played_modes']:
+            continue
+        mdstr += f"\n| {key} | {userdata_pl4.get(key,'未找到')} | {userdata_pl3.get(key,'未找到')} |"
+    return await md_to_pic(mdstr,css_path=Path(__file__).parent / Path("template/style.css"),width=700)
--- query/utils.py
+++ query/utils.py
@@ -1,8 +1,4 @@
 from typing import Tuple
-from PIL import Image, ImageDraw, ImageFont
-from pathlib import Path
-from random import choice
-from io import BytesIO
 
 
 
@@ -27,25 +23,47 @@ def majsoul_level_count(id:int,score:int,delta:int) -> Tuple[str,int,int]:
         10704:{"level":"魂天4","origin_score":1000,"max_score":2000},
         10705:{"level":"魂天5","origin_score":1000,"max_score":2000},
         10706:{"level":"魂天6","origin_score":1000,"max_score":2000},
-        10707:{"level":"魂天7","origin_score":1000,"max_score":2000}}
+        10707:{"level":"魂天7","origin_score":1000,"max_score":2000},
+        10708:{"level":"魂天8","origin_score":1000,"max_score":2000},
+        10709:{"level":"魂天9","origin_score":1000,"max_score":2000},
+        10710:{"level":"魂天10","origin_score":1000,"max_score":2000},
+
+        20301:{"level":"雀杰1","origin_score":600,"max_score":1200},
+        20302:{"level":"雀杰2","origin_score":700,"max_score":1400},
+        20303:{"level":"雀杰3","origin_score":1000,"max_score":2000},
+        20401:{"level":"雀豪1","origin_score":1400,"max_score":2800},
+        20402:{"level":"雀豪2","origin_score":1600,"max_score":3200},
+        20403:{"level":"雀豪3","origin_score":1800,"max_score":3600},
+        20501:{"level":"雀圣1","origin_score":2000,"max_score":4000},
+        20502:{"level":"雀圣2","origin_score":3000,"max_score":6000},
+        20503:{"level":"雀圣3","origin_score":4500,"max_score":9000},
+        20701:{"level":"魂天1","origin_score":1000,"max_score":2000},
+        20702:{"level":"魂天2","origin_score":1000,"max_score":2000},
+        20703:{"level":"魂天3","origin_score":1000,"max_score":2000},
+        20704:{"level":"魂天4","origin_score":1000,"max_score":2000},
+        20705:{"level":"魂天5","origin_score":1000,"max_score":2000},
+        20706:{"level":"魂天6","origin_score":1000,"max_score":2000},
+        20707:{"level":"魂天7","origin_score":1000,"max_score":2000},
+        20708:{"level":"魂天8","origin_score":1000,"max_score":2000},
+        20709:{"level":"魂天9","origin_score":1000,"max_score":2000},
+        20710:{"level":"魂天10","origin_score":1000,"max_score":2000},
+        20711:{"level":"魂天11","origin_score":1000,"max_score":2000},
+        20712:{"level":"魂天12","origin_score":1000,"max_score":2000},
+        20713:{"level":"魂天13","origin_score":1000,"max_score":2000},
+        20714:{"level":"魂天14","origin_score":1000,"max_score":2000},
+        20715:{"level":"魂天15","origin_score":1000,"max_score":2000},
+        20716:{"level":"魂天16","origin_score":1000,"max_score":2000},
+        20717:{"level":"魂天17","origin_score":1000,"max_score":2000},
+        20718:{"level":"魂天18","origin_score":1000,"max_score":2000},
+        20719:{"level":"魂天19","origin_score":1000,"max_score":2000},
+        20720:{"level":"魂天20","origin_score":1000,"max_score":2000}}
     
     # 掉段特殊情况(只考虑掉雀士)
     if id==10301 and score+delta<0:
+        return ("雀士3",500,1000)
+    if id==20301 and score+delta<0:
         return ("雀士3",500,1000)
     
     level_info = majsoul_level_map[id]
     true_socre = score+delta
     return (level_info["level"], true_socre, level_info["max_score"])
-
-def img_util() -> Image.Image:
-    img_path = Path(__file__).parent / Path("img/")
-    background = Image.open(choice(list(img_path.iterdir())),'r')  # 请使用768*1024的图片
-
-    draw = ImageDraw.Draw(background)
-    draw.rectangle((100,200,100,200),"white")
-
-    return background
-
-
-if __name__=='__main__':
-    img_util().show()
